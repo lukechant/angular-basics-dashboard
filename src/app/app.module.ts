8fcee5d99515887efb1c28bef9b7cd480b8b67ff
luke-chant <luke.chant@autotrader.co.uk>
2021-06-24 09:44:40 +0100

    chaged to stales.css to make app load properly

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
